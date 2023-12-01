c53a092924131d4cd8e82daf287f711b6fe811dc
parksey <parkseyeon99@naver.com>
2023-12-01 23:53:57 +0900

    feat: 수정

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store.js'
+import store from './store'
 
 import CoreuiVue from '@coreui/vue'
 import CIcon from '@coreui/icons-vue'
